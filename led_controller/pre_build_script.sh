78b1fdb6df52e06968e17ebe1953b74664d42bff
mldxo <miloszmaculewicz@gmail.com>
2024-06-11 20:43:18 +0200

    Update script

--- led_controller/pre_build_script.sh
+++ led_controller/pre_build_script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-WIFI_SSID=$1
-WIFI_PASSWORD=$2
+SCRIPT_DIR=$(dirname "$0")
+LED_CONTROLLER_PATH=$(readlink -f "$SCRIPT_DIR/../led_controller/led_controller.c")
+
+WIFI_SSID=$(printenv WIFI_SSID)
+WIFI_PASSWORD=$(printenv WIFI_PASSWORD)
 
 if [ -z "$WIFI_SSID" ] || [ -z "$WIFI_PASSWORD" ]; then
     echo "Warning: Both WIFI_SSID and WIFI_PASSWORD must be provided as arguments."
@@ -10,6 +13,6 @@ fi
 git stash push -m "Pre-build stash"
 
 if [ ! -z "$WIFI_SSID" ] && [ ! -z "$WIFI_PASSWORD" ]; then
-    sed -i "s/wifi_ssid/${WIFI_SSID}/g" led_controller.c
-    sed -i "s/wifi_password/${WIFI_PASSWORD}/g" led_controller.c
+    sed -i "s/wifi_ssid/${WIFI_SSID}/g" $LED_CONTROLLER_PATH
+    sed -i "s/wifi_password/${WIFI_PASSWORD}/g" $LED_CONTROLLER_PATH
 fi
