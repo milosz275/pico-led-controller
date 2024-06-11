f585ace8568974484e55e82d5ee726176cd28dbe
mldxo <miloszmaculewicz@gmail.com>
2024-06-11 21:50:50 +0200

    Update scripts

--- led_controller/post_build_script.sh
+++ led_controller/post_build_script.sh
@@ -4,11 +4,8 @@ SCRIPT_DIR=$(dirname "$0")
 LED_CONTROLLER_PATH=$(readlink -f "$SCRIPT_DIR/../led_controller/led_controller.c")
 
 if [ -f "$LED_CONTROLLER_PATH" ]; then
-    if [ $(git stash list | wc -l) -gt 0 ]; then
-        git checkout stash@{0} -- "$LED_CONTROLLER_PATH"
-    else
-        echo "No stashes available to apply."
-    fi
+    sed -i "s|${WIFI_SSID}|wifi_ssid|g" $LED_CONTROLLER_PATH
+    sed -i "s|${WIFI_PASSWORD}|wifi_password|g" $LED_CONTROLLER_PATH
 else
     echo "Error: led_controller.c not found at $LED_CONTROLLER_PATH"
 fi
--- led_controller/pre_build_script.sh
+++ led_controller/pre_build_script.sh
@@ -8,11 +8,7 @@ WIFI_PASSWORD=$(printenv WIFI_PASSWORD)
 
 if [ -z "$WIFI_SSID" ] || [ -z "$WIFI_PASSWORD" ]; then
     echo "Warning: Both WIFI_SSID and WIFI_PASSWORD must be provided as arguments."
-fi
-
-git stash push -m "Pre-build stash"
-
-if [ ! -z "$WIFI_SSID" ] && [ ! -z "$WIFI_PASSWORD" ]; then
+else
     sed -i "s|wifi_ssid|${WIFI_SSID}|g" $LED_CONTROLLER_PATH
     sed -i "s|wifi_password|${WIFI_PASSWORD}|g" $LED_CONTROLLER_PATH
 fi
