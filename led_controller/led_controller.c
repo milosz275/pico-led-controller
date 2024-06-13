331cb56d57990ba2b6c51fd83a1deeb05a831981
mldxo <miloszmaculewicz@gmail.com>
2024-06-13 17:17:57 +0200

    Add light button toggle

--- led_controller/led_controller.c
+++ led_controller/led_controller.c
@@ -6,6 +6,7 @@
 #include <pico/cyw43_arch.h>
 #include <boards/pico_w.h>
 #include <hardware/pio.h>
+#include <hardware/gpio.h>
 #include <hardware/clocks.h>
 
 #include "urgb.h"
@@ -19,13 +20,13 @@
 // #include "ssi.h"
 // #include "cgi.h"
 
-#define LIGHT_TOGGLE_PIN 1
 #define WS2812_PIN 2
+#define LIGHT_TOGGLE_PIN 15
 #define NUM_PIXELS 118
 #define IS_RGBW false
 
 bool light_on = true;
-bool button_pressed_prev = true;
+bool button_previous_state = true; // button is up
 
 void onboard_led_blink(int on_time, int off_time)
 {
@@ -35,9 +36,21 @@ void onboard_led_blink(int on_time, int off_time)
     sleep_ms(off_time);
 }
 
+void connect_to_wifi()
+{
+    printf("Connecting to Wi-Fi...\n");
+    while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000) != 0)
+        printf("Attempting to connect...\n");
+    printf("Connected!\n");
+}
+
 bool init()
 {
     stdio_init_all();
+    gpio_init(LIGHT_TOGGLE_PIN);
+    gpio_set_dir(LIGHT_TOGGLE_PIN, GPIO_IN);
+    gpio_pull_up(LIGHT_TOGGLE_PIN);
+
     if (cyw43_arch_init())
     {
         printf("Wi-Fi init failed");
@@ -45,11 +58,10 @@ bool init()
     }
 
     cyw43_arch_enable_sta_mode();
-    while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000) != 0) // ten seconds timeout
-        printf("Attempting to connect...\n");
-    printf("Connected!\n");
+    connect_to_wifi();
+    
     onboard_led_blink(250, 50);
-    onboard_led_blink(250, 0);
+    onboard_led_blink(250, 50);
     return true;
 }
 
@@ -65,6 +77,22 @@ void apply_rainbow_effect(int *base_hue, int *speed_factor, int *density_factor,
     *speed_factor = rand() % 3 + 5;
     *base_hue += *speed_factor;
     *base_hue %= 360;
+}
+
+void handle_light_toggle()
+{
+    if (gpio_get(LIGHT_TOGGLE_PIN))
+        if (!button_previous_state)
+            button_previous_state = true;
+    else
+    {
+        if (button_previous_state)
+        {
+            light_on = !light_on;
+            button_previous_state = false;
+            printf("Light toggled");
+        }
+    }
 }
 
 int main()
@@ -85,7 +113,14 @@ int main()
     int brightness = 100;
     while (true)
     {
-        onboard_led_blink(50, 10);
-        apply_rainbow_effect(&base_hue, &speed_factor, &density_factor, &brightness);
+        handle_light_toggle();
+        if (!light_on)
+            turn_off_all(NUM_PIXELS);
+        else
+            apply_rainbow_effect(&base_hue, &speed_factor, &density_factor, &brightness);
+        if (cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA) != CYW43_LINK_UP)
+            connect_to_wifi();
+        sleep_ms(50);
     }
+    cyw43_arch_deinit();
 }
