7a65b2d29f40b96beb4367138fe79668306c49a3
mldxo <miloszmaculewicz@gmail.com>
2024-06-13 23:33:06 +0200

    Adjust light state functionality

--- led_controller/light_state.h
+++ led_controller/light_state.h
@@ -1,11 +1,24 @@
 #ifndef _LIGHT_STATE_H
 #define _LIGHT_STATE_H
 
-bool light_state = true;
+#define NUM_PIXELS 118
+
+#include <stdint.h>
+
+volatile bool light_state = true;
 
 void toggle_light_state()
 {
     light_state = !light_state;
+    if (!light_state)
+        turn_off_all(NUM_PIXELS);
 }
+
+// extern struct light_state_t
+// {
+//     bool state;
+//     uint8_t brightness;
+//     uint8_t lighting_mode;
+// } light_state;
 
 #endif
