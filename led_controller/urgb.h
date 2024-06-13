d2dbf999f3ea74d7d2f19cdef959d4acbfed661d
mldxo <miloszmaculewicz@gmail.com>
2024-06-13 12:25:43 +0200

    Add urgb header

---
+++ led_controller/urgb.h
@@ -0,0 +1,14 @@
+#ifndef _URGB_H
+#define _URGB_H
+
+#include <stdint.h>
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return
+        ((uint32_t) (r) << 8) |
+        ((uint32_t) (g) << 16) |
+        (uint32_t) (b);
+}
+
+#endif
