d8213059d33678b4c1a0f438e7a4cf3ac81ab63e
mldxo <miloszmaculewicz@gmail.com>
2024-06-14 18:01:51 +0200

    Add too low sleep time check

--- led_controller/blink.h
+++ led_controller/blink.h
@@ -10,6 +10,10 @@
 
 void onboard_led_blink(uint32_t on_time, uint32_t off_time)
 {
+    if (on_time < 10)
+        on_time = 10;
+    if (off_time < 10)
+        off_time = 10;
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     sleep_ms(on_time);
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
@@ -18,6 +22,10 @@ void onboard_led_blink(uint32_t on_time, uint32_t off_time)
 
 void led_blink(uint32_t on_time, uint32_t off_time)
 {
+    if (on_time < 10)
+        on_time = 10;
+    if (off_time < 10)
+        off_time = 10;
     toggle_light_state();
     sleep_ms(on_time);
     toggle_light_state();
