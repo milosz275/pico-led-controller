785cf0dadfafaeb49eb3c59bb8060192b89e4f6d
mldxo <miloszmaculewicz@gmail.com>
2024-06-16 00:27:03 +0200

    Add mode data fetch, add network abort on client-side actions

--- led_controller/public/app.js
+++ led_controller/public/app.js
@@ -1,25 +1,38 @@
+let fetchController = new AbortController();
+
 function fetchData() {
     fetch("/data.shtml")
         .then(response => response.text())
         .then(data => {
-            const [volt, temp, onboardled, led] = data.trim().split("\n");
+            const [volt, temp, onboardled, led, mode] = data.trim().split("\n");
 
             const placeholders = {
                 volt: "<!--#volt-->",
                 temp: "<!--#temp-->",
                 onboardled: "<!--#onboardled-->",
-                led: "<!--#led-->"
+                led: "<!--#led-->",
+                mode: "<!--#mode-->"
             };
 
             document.getElementById("volt").innerText = (volt !== placeholders.volt) ? volt + " V" : "N/A";
             document.getElementById("temp").innerText = (temp !== placeholders.temp) ? temp + " °C" : "N/A";
             document.getElementById("onboardled").innerText = (onboardled !== placeholders.onboardled) ? onboardled : "N/A";
             document.getElementById("led").innerText = (led !== placeholders.led) ? led : "N/A";
+            document.getElementById("mode").innerText = (mode !== placeholders.mode) ? mode : "N/A";
+
+            const modeSelect = document.getElementById("modeSelect");
+            if (mode !== placeholders.mode && modeSelect) {
+                modeSelect.value = mode;
+            } else {
+                modeSelect.value = "rainbow-wheel";
+            }
         })
         .catch(error => console.error("Error fetching data:", error));
 }
 
 function toggleOnboardLED() {
+    fetchController.abort();
+    fetchController = new AbortController();
     fetch("/onboard_led?toggle")
         .then(response => {
             if (!response.ok) {
@@ -35,6 +48,8 @@ function toggleOnboardLED() {
 }
 
 function toggleLEDStrip() {
+    fetchController.abort();
+    fetchController = new AbortController();
     fetch("/led?toggle")
         .then(response => {
             if (!response.ok) {
@@ -53,21 +68,15 @@ setInterval(fetchData, 5000);
 
 document.addEventListener("DOMContentLoaded", fetchData);
 
-function fetchWithTimeout(url, options, timeout = 30000) {
-    return Promise.race([
-        fetch(url, options),
-        new Promise((_, reject) =>
-            setTimeout(() => reject(new Error("Timeout")), timeout)
-        )
-    ]);
-}
-
 function changeLEDMood(mood) {
+    fetchController.abort();
+    fetchController = new AbortController();
     const url = `/mode?${encodeURIComponent(mood)}`;
     fetch(url)
     .then(response => {
         if (response.ok) {
             console.log(`LED mood changed to ${mood}`);
+            fetchData();
         } else {
             console.error("Failed to change LED mood");
         }
