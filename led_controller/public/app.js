819b4bf78bf9c0b22349d696cd8428218536161a
mldxo <miloszmaculewicz@gmail.com>
2024-06-17 14:35:58 +0200

    Add start timestamp and time since start

--- led_controller/public/app.js
+++ led_controller/public/app.js
@@ -83,3 +83,67 @@ function changeLEDMood(mood) {
     })
     .catch(error => console.error("Error:", error));
 }
+
+function updateElapsedTime() {
+    const tmElement = document.getElementById("tm");
+    const timestamp = tmElement.getAttribute("data-timestamp");
+    if (!timestamp) return;
+
+    const timestampDate = new Date(timestamp).getTime();
+
+    setInterval(() => {
+        const now = Date.now();
+        // Adjust the elapsed time by adding the timezone offset
+        let elapsed = now - timestampDate;
+
+        const hours = Math.floor(elapsed / (1000 * 60 * 60));
+        elapsed -= hours * (1000 * 60 * 60);
+        const minutes = Math.floor(elapsed / (1000 * 60));
+        elapsed -= minutes * (1000 * 60);
+        const seconds = Math.floor(elapsed / 1000);
+
+        const formattedElapsedTime = [
+            hours.toString().padStart(2, '0'),
+            minutes.toString().padStart(2, '0'),
+            seconds.toString().padStart(2, '0')
+        ].join(':');
+
+        document.getElementById("elapsed").innerText = `${formattedElapsedTime}`;
+    }, 1000);
+}
+
+function fetchTimestamp() {
+    fetch("/timestamp.shtml")
+        .then(response => response.text())
+        .then(data => {
+            const [tm] = data.trim().split("\n");
+
+            const placeholders = {
+                tm: "<!--#tm-->"
+            };
+
+            if (tm !== placeholders.tm) {
+                const date = new Date(tm);
+                const formattedDate = [
+                    date.getMonth() + 1,
+                    date.getDate(),
+                    date.getFullYear().toString().slice(-2)
+                ].join('-') + ' ' +
+                [
+                    date.getHours().toString().padStart(2, '0'),
+                    date.getMinutes().toString().padStart(2, '0'),
+                    date.getSeconds().toString().padStart(2, '0')
+                ].join(':');
+
+                const tmElement = document.getElementById("tm");
+                tmElement.innerText = formattedDate;
+                tmElement.setAttribute("data-timestamp", tm); // Set timestamp for later use
+                updateElapsedTime(); // Call the function to start updating elapsed time
+            } else {
+                document.getElementById("tm").innerText = "N/A";
+            }
+        })
+        .catch(error => console.error("Error fetching data:", error));
+}
+
+document.addEventListener("DOMContentLoaded", fetchTimestamp);
