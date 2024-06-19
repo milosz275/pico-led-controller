df6c1d7bade54fddca7ec21ceea1f4b51e3e9930
mldxo <miloszmaculewicz@gmail.com>
2024-06-19 23:46:52 +0200

    Use define instead of static inline

--- led_controller/include/urgb.h
+++ led_controller/include/urgb.h
@@ -3,12 +3,6 @@
 
 #include <stdio.h>
 
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
-{
-    return
-        ((uint32_t) (r) << 8) |
-        ((uint32_t) (g) << 16) |
-        (uint32_t) (b);
-}
+#define URGB(r, g, b) ((uint32_t) (r) << 8) | ((uint32_t) (g) << 16) | (uint32_t) (b)
 
 #endif
