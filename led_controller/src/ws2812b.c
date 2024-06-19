9939e73a1514c306befafa2f3c522e70ff3f7d20
mldxo <miloszmaculewicz@gmail.com>
2024-06-19 23:47:38 +0200

    Add define usage

--- led_controller/src/ws2812b.c
+++ led_controller/src/ws2812b.c
@@ -21,31 +21,31 @@ void set_all_leds(uint32_t color, uint len)
 
 void turn_off_all(uint len)
 {
-    uint32_t off = urgb_u32(0, 0, 0);
+    uint32_t off = URGB(0, 0, 0);
     set_all_leds(off, len);
 }
 
 void set_all_red(uint len, uint8_t brightness)
 {
-    uint32_t red = urgb_u32(brightness, 0, 0);
+    uint32_t red = URGB(brightness, 0, 0);
     set_all_leds(red, len);
 }
 
 void set_all_green(uint len, uint8_t brightness)
 {
-    uint32_t green = urgb_u32(0, brightness, 0);
+    uint32_t green = URGB(0, brightness, 0);
     set_all_leds(green, len);
 }
 
 void set_all_blue(uint len, uint8_t brightness)
 {
-    uint32_t blue = urgb_u32(0, 0, brightness);
+    uint32_t blue = URGB(0, 0, brightness);
     set_all_leds(blue, len);
 }
 
 void set_all_white(uint len, uint8_t brightness)
 {
-    uint32_t white = urgb_u32(brightness, brightness, brightness);
+    uint32_t white = URGB(brightness, brightness, brightness);
     set_all_leds(white, len);
 }
 
@@ -60,43 +60,43 @@ void set_rainbow_spectrum(uint len)
 
 void set_all_purple(uint len, uint8_t brightness)
 {
-    uint32_t purple = urgb_u32(brightness * 3 / 4, 0, brightness);
+    uint32_t purple = URGB(brightness * 3 / 4, 0, brightness);
     set_all_leds(purple, len);
 }
 
 void set_all_yellow(uint len, uint8_t brightness)
 {
-    uint32_t yellow = urgb_u32(brightness, brightness, 0);
+    uint32_t yellow = URGB(brightness, brightness, 0);
     set_all_leds(yellow, len);
 }
 
 void set_all_cyan(uint len, uint8_t brightness)
 {
-    uint32_t cyan = urgb_u32(0, brightness, brightness);
+    uint32_t cyan = URGB(0, brightness, brightness);
     set_all_leds(cyan, len);
 }
 
 void set_all_orange(uint len, uint8_t brightness)
 {
-    uint32_t orange = urgb_u32(brightness, brightness / 3, 0);
+    uint32_t orange = URGB(brightness, brightness / 3, 0);
     set_all_leds(orange, len);
 }
 
 void set_all_pink(uint len, uint8_t brightness)
 {
-    uint32_t pink = urgb_u32(brightness, brightness / 3, brightness / 3);
+    uint32_t pink = URGB(brightness, brightness / 3, brightness / 3);
     set_all_leds(pink, len);
 }
 
 void set_all_turquoise(uint len, uint8_t brightness)
 {
-    uint32_t turquoise = urgb_u32(brightness / 3, brightness * 2 / 3, brightness);
+    uint32_t turquoise = URGB(brightness / 3, brightness * 2 / 3, brightness);
     set_all_leds(turquoise, len);
 }
 
 void set_all_magenta(uint len, uint8_t brightness)
 {
-    uint32_t magenta = urgb_u32(brightness, 0, brightness);
+    uint32_t magenta = URGB(brightness, 0, brightness);
     set_all_leds(magenta, len);
 }
 
