e6f5fc5d408498f33fb078956ac9c901fb2b1690
mldxo <miloszmaculewicz@gmail.com>
2024-06-27 12:04:39 +0200

    Add URGB to R, G, B convertion

--- led_controller/include/urgb.h
+++ led_controller/include/urgb.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 #define URGB(r, g, b) ((uint32_t) (r) << 8) | ((uint32_t) (g) << 16) | (uint32_t) (b)
+#define URGB_R(color) ((color >> 8) & 0xFF)
+#define URGB_G(color) ((color >> 16) & 0xFF)
+#define URGB_B(color) (color & 0xFF)
 
 #endif
