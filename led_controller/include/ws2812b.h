fb2e2c4c4207d74d013c596a9950f5a8cfcb051e
mldxo <miloszmaculewicz@gmail.com>
2024-06-27 12:04:49 +0200

    Add color getters

--- led_controller/include/ws2812b.h
+++ led_controller/include/ws2812b.h
@@ -2,6 +2,7 @@
 #define _WS2812B_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * Put pixel to the LED strip.
@@ -24,6 +25,94 @@ void set_all_leds(uint32_t color, uint len);
  * @param len: number of leds
  */
 void turn_off_all(uint len);
+
+/**
+ * Get red color.
+ * 
+ * @param brightness: brightness
+ * @return red color in RGB format
+ */
+uint32_t get_red(uint8_t brightness);
+
+/**
+ * Get green color.
+ * 
+ * @param brightness: brightness
+ * @return green color in RGB format
+ */
+uint32_t get_green(uint8_t brightness);
+
+/**
+ * Get blue color.
+ * 
+ * @param brightness: brightness
+ * @return blue color in RGB format
+ */
+uint32_t get_blue(uint8_t brightness);
+
+/**
+ * Get white color.
+ * 
+ * @param brightness: brightness
+ * @return white color in RGB format
+ */
+uint32_t get_white(uint8_t brightness);
+
+/**
+ * Get purple color.
+ * 
+ * @param brightness: brightness
+ * @return purple color in RGB format
+ */
+uint32_t get_purple(uint8_t brightness);
+
+/**
+ * Get yellow color.
+ * 
+ * @param brightness: brightness
+ * @return yellow color in RGB format
+ */
+uint32_t get_yellow(uint8_t brightness);
+
+/**
+ * Get cyan color.
+ * 
+ * @param brightness: brightness
+ * @return cyan color in RGB format
+ */
+uint32_t get_cyan(uint8_t brightness);
+
+/**
+ * Get orange color.
+ * 
+ * @param brightness: brightness
+ * @return orange color in RGB format
+ */
+uint32_t get_orange(uint8_t brightness);
+
+/**
+ * Get pink color.
+ * 
+ * @param brightness: brightness
+ * @return pink color in RGB format
+ */
+uint32_t get_pink(uint8_t brightness);
+
+/**
+ * Get turquoise color.
+ * 
+ * @param brightness: brightness
+ * @return turquoise color in RGB format
+ */
+uint32_t get_turquoise(uint8_t brightness);
+
+/**
+ * Get magenta color.
+ * 
+ * @param brightness: brightness
+ * @return magenta color in RGB format
+ */
+uint32_t get_magenta(uint8_t brightness);
 
 /**
  * Set all leds to red.
@@ -127,8 +216,9 @@ void set_all_magenta(uint len, uint8_t brightness);
  * @param base_hue: base hue
  * @param speed_factor: speed factor
  * @param density_factor: density factor
+ * @param brightness: brightness
  */
-void apply_rainbow_wheel_effect(uint len, uint16_t* base_hue, uint8_t* speed_factor, uint8_t* density_factor);
+void apply_rainbow_wheel_effect(uint len, uint16_t* base_hue, uint8_t* speed_factor, uint8_t* density_factor, volatile uint8_t* brightness);
 
 /**
  * Apply rainbow cycle effect.
@@ -136,7 +226,29 @@ void apply_rainbow_wheel_effect(uint len, uint16_t* base_hue, uint8_t* speed_fac
  * @param len: number of leds
  * @param hue: hue
  * @param speed_factor: speed factor
+ * @param brightness: brightness
  */
-void apply_rainbow_cycle_effect(uint len, uint16_t* hue, uint8_t* speed_factor);
+void apply_rainbow_cycle_effect(uint len, uint16_t* hue, uint8_t* speed_factor, volatile uint8_t* brightness);
+
+/**
+ * Apply breathing effect.
+ * 
+ * @param len: number of leds
+ * @param speed_factor: speed factor
+ * @param color: color
+ * @param base_brightness: base brightness
+ * @param brightness: brightness
+ * @param breathing_up: breathing up
+ */
+void apply_breathing_effect(uint len, uint8_t* speed_factor, uint32_t* color, volatile uint8_t* base_brightness, uint8_t* brightness, bool* breathing_up);
+
+/**
+ * Apply flashing effect.
+ * 
+ * @param len: number of leds
+ * @param led_values: led values
+ * @param color: color
+ */
+void apply_flashing_effect(uint len, uint32_t* color);
 
 #endif
